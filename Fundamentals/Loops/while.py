7f73bbf1e362200dd3d91295392b81799971139a
WSL <tartaacanon@gmail.com>
2023-01-24 03:10:57 -0500

    Variables and loops

---
+++ Fundamentals/Loops/while.py
@@ -0,0 +1,5 @@
+i = 1
+
+while i < 11:
+    print(f'pablo escobar: {i}')
+    i += 1
---
+++ Fundamentals/Variables/var.py
@@ -0,0 +1,7 @@
+age = 19
+movie = 'spiderman'
+main_character = 'peter parker'
+
+print(f'age of {movie} = {age} and his name is {main_character}')
+
+
