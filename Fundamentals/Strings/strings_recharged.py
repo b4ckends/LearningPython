222d6fde9bfbbb5c6cbfe3baba581cd987f960ae
WSL <tartaacanon@gmail.com>
2023-02-08 13:57:44 -0500

    Indexing, slicing and recharged string

---
+++ Fundamentals/Strings/strings_recharged.py
@@ -0,0 +1,12 @@
+text = 'elon relon loren musrk'
+print('Javascript' in text)
+print('elon' in text)
+
+if 'elon' in text:
+    print('musk is in the text')
+
+size = len('alksfjadjflasdjkajdljfjdfd38')
+print(f'This text has = {size} letetres')
+
+espacieeee = len('a                  ')
+print(f'This text with spaces has = {espacieeee} letters')
