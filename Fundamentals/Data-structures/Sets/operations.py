750735f2354ed4e2e6e192864a8a1263c21204c8
WSL <tartaacanon@gmail.com>
2023-07-31 01:33:29 -0500

    Renamed: Proyects to Programs

---
+++ Fundamentals/Data-structures/Sets/operations.py
@@ -0,0 +1,8 @@
+set_a = {'col', 'mex', 'bol'}
+set_b = {'pe', 'bol'}
+
+set_c = (set_a.union(set_b))
+
+print(f'a way to unifique sets = {set_c}')
+
+print(f'another way to do this = {set_a | set_b}')
